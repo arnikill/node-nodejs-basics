d0786758320ddc177924246d1bc7c533c211f26f
arnikill <tusupkalievaaliya921@gmail.com>
2025-04-28 20:58:16 +0600

    refactor: rewrite argument parsing using forEach

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,12 +1,15 @@
 const parseArgs = () => {
+
     const args = process.argv.slice(2)
     const result = []
-    for (let i = 0; i < args.length; i += 2) {
-        const propName = args[i].slice(2);
-        const propValue = args[i + 1]
-        result.push(`${propName} is ${propValue}`)
-    }
-    console.log(result.join(','))
+    args.forEach((arg,index) => {
+        if(index % 2 === 0){
+            const propName = arg.slice(2)
+            const propValue = args[index + 1]
+            result.push(`${propName} is ${propValue}`)
+        }
+    })
+    console.log(result.join(','));
 };
 
 parseArgs();
