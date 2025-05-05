1333e64f269f8a210a685305e6c7bc74aca82352
arnikill <tusupkalievaaliya921@gmail.com>
2025-05-05 19:14:38 +0600

    Add calcHash.js to compute SHA256 hash for a file

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,24 +1,33 @@
 //calcHash.js- реализовать функцию, которая вычисляет хэш SHA256 для файла fileToCalculateHashFor.txt и записывает его в консоль как  hex
-import { createReadStream } from 'fs';
+import { access, createReadStream } from 'fs';
 import { createHash } from 'crypto'
+import path from 'path';
+import { fileURLToPath } from 'url';
 const calculateHash = async () => {
-    const path = './files/fileToCalculateHashFor.txt'
-    const hash = createHash('SHA256')
-    const stream = createReadStream(path)
 
-    return new Promise((resolve, reject) => {
-        stream.on('error', (err) => {
-            reject(err)
-        })
-        stream.on('data', (chunk) => {
-            hash.update(chunk)
-        })
-        stream.on('end', () => {
-            const hexHash = hash.digest('hex')
-            console.log(`hash for ${path}: ${hexHash}`)
-            resolve(hexHash)
-        })
-    })
+    const __filename = fileURLToPath(import.meta.url)
+    const __dirname =  path.dirname(__filename)
+    const pathToFile = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt')
+    const hash = createHash('SHA256')
+    
+    try{
+       const stream = createReadStream(pathToFile)
+
+       for await(const chunk of stream){
+        hash.update(chunk)
+       }
+       
+       const hexHash = hash.digest('hex')
+       console.log(`hash for ${pathToFile}: ${hexHash}`)
+       return hexHash
+
+    }catch (err){
+        if(err.code === 'ENOENT'){
+            console.error('file does not exist')
+        }else{
+            throw new Error('FS operation is failed')
+        }
+    }
 };
 
 await calculateHash();
