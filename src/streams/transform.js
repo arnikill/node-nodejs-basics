a74863ad21889bf921a903687922ccc79a561537
arnikill <tusupkalievaaliya921@gmail.com>
2025-05-09 22:23:43 +0300

    refactor: cleaning and improving the code structure

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,21 +1,23 @@
 //transform.js- реализовать функцию, которая считывает данные из process.stdin, переворачивает текст с помощью Transform Stream и затем записывает его в process.stdout
 import { Transform } from 'stream';
+import { pipeline } from 'stream/promises';
 const transform = async () => {
-    return new Promise((resolve, reject) => {
-        const reverseStream = Transform({
-            transform(chunk, encoding, callback) {
+        const reverseStream = new Transform({
+            transform(chunk,encoding,callback) {
                 const reverseChunk = chunk.toString().split('').reverse().join('')
-                this.push(reverseChunk)
-                callback()
+                callback(null, reverseChunk)
             }
         })
-        process.stdin.pipe(reverseStream).pipe(process.stdout)
-        process.stdin.on('end', () => {
-            resolve()
-        })
-        process.stdout.on('error', (err) => {
-            reject(err)
-        })
-    })
+       
+        try{
+            await pipeline(
+                process.stdin,
+                reverseStream,
+                process.stdout
+            )
+            process
+        }catch(error){
+            console.error('Operation failed')
+        }
 }
 await transform();
